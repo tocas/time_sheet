3b252fb27f996b5dc7560aff5a674e93ac8b6986
Tomas Lucovic <tomaslucovic@gmail.com>
2011-03-13 10:48:22 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeSheet::Application.config.secret_token = '64393f16868847576086857cecacb59ce23a6a6815a70fe5841aec9132b0466f1fab54a9f2d09e40c114d21a66ce4c49f436afdb146ee7554576a2906e2cf0b8'
