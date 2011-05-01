dc0b566ab7feed69faca4fa81d5dc63b86bb2043
Tomas Lucovic <tomaslucovic@gmail.com>
2011-05-02 01:07:17 +0200

    add devise login

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,3 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-TimeSheet::Application.config.secret_token = '64393f16868847576086857cecacb59ce23a6a6815a70fe5841aec9132b0466f1fab54a9f2d09e40c114d21a66ce4c49f436afdb146ee7554576a2906e2cf0b8'
