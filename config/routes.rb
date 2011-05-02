e851a2fb5c7e298b35de7bdcf7a165befd92d5de
Tomas Lucovic <tomaslucovic@gmail.com>
2011-05-02 14:01:49 +0200

    add static page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 TimeSheet::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"  
+
   devise_for :users
 
   # The priority is based upon order of creation:
@@ -50,8 +54,7 @@ TimeSheet::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "welcome#index"
-
+  root :to => 'pages#home'  
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
