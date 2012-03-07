744fa22f51045fd1f598ec8e1dfff3fc87672fa8
Tomas Lucovic <tomaslucovic@gmail.com>
2012-03-07 18:18:10 +0100

    Add function to find user

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,7 @@
 class ApiController < ApplicationController
   def create
     post_data = request.body.read
-    @user = User.first
+    @user = User.where(["email = ?", params[:user]]).first
     @work = @user.works.build(JSON.parse(post_data))
     if @work.save 
       logger.debug "User saved"
